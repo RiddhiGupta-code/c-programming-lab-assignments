815425fa6e30b620189155a80694ada271f2ff3c
RiddhiGupta-code <riddhigupta7002@gmail.com>
2026-04-12 10:02:34 +0000

    binary to decimal

---
+++ Assignment14[binarytodecimal].c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    long long binary;
+    int decimal=0,base=1,remainder;
+    printf("Enter binary number: ");
+    scanf("%lld",&binary);
+    while(binary>0){
+        remainder=binary%10;
+        decimal=decimal+remainder*base;
+        binary=binary/10;
+        base=base*2;
+
+    }
+    printf("decimal number=%d",decimal);
+}
